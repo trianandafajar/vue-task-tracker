eb3e267805d81c9bed96d0802f17430b39d35427
trianandafajar <mail.trianandafajar@gmail.com>
2025-02-05 09:45:35 +0700

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <header>
+        <h1>{{title}}</h1>
+        <Button 
+            v-show="isHomePage"
+            :text="showAddTask ? 'Close' : 'Add Task'" 
+            @btn-click="$emit('toggle-add-task')" 
+            :color="showAddTask ? 'red' : 'green' "
+        />
+    </header>
+</template>
+
+<script>
+import Button from "./Button.vue";
+export default {
+    name: "Header",
+    props: {
+        title: String,
+        showAddTask: Boolean
+    },
+    emits: [
+        'btn-click'
+    ],
+    components: { Button },
+    computed: {
+        isHomePage() {
+            return this.$route.path === '/'
+        }
+    }
+}
+</script>
+
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
